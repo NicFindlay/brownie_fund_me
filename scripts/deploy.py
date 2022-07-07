45b10736e3d1dc726bef9ada047c7c98bcce0411
Nic <nicisme@gmail.com>
2022-07-07 15:55:54 +0200

    Price feeds and mock price feeds implementation

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,28 @@
-from brownie import FundMe
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful_scripts import get_account, deploy_mocks
+from web3 import Web3
 
 
 def deploy_fund_me():
     account = get_account()
+    # pass the price feed address to our fundme contract
+
+    # if we are on a persistent network, use associated address
+    # otherwise use mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_used_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        print("Mocks Deployed")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"Contract deployed to {fund_me.address}")
 
 
 def main():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,8 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 18
+STARTING_PRICE = 2000
 
 
 def get_account():
@@ -6,3 +10,13 @@ def get_account():
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The actiove network is {network.show_active()}")
+    print("Deploying mocks")
+    if len(MockV3Aggregator) <= 0:
+        mock_aggregator = MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": account}
+        )
+    price_feed_address = MockV3Aggregator[-1].address
