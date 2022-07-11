6aba039ca77b9bb305c8a7079c06036acbdb852b
Nic <nicisme@gmail.com>
2022-07-11 17:42:20 +0200

    Mainnet Fork Development environment Completed

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -5,10 +5,14 @@ DECIMALS = 8
 STARTING_PRICE = 20000000000000
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
