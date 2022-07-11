062d6705a95c2dd51c88f06729c7050acc33d921
Nic <nicisme@gmail.com>
2022-07-11 17:30:13 +0200

    Withdraw and fun functions. Pytest included

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,9 @@
 from brownie import FundMe, network, config, MockV3Aggregator
-from scripts.helpful_scripts import get_account, deploy_mocks
-from web3 import Web3
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
@@ -9,13 +12,14 @@ def deploy_fund_me():
 
     # if we are on a persistent network, use associated address
     # otherwise use mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_used_price_feed"
         ]
     else:
         deploy_mocks()
         print("Mocks Deployed")
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
@@ -23,6 +27,7 @@ def deploy_fund_me():
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,22 +1,24 @@
 from brownie import network, config, accounts, MockV3Aggregator
 from web3 import Web3
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 20000000000000
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
 
 
 def deploy_mocks():
-    print(f"The actiove network is {network.show_active()}")
+    print(f"The active network is {network.show_active()}")
     print("Deploying mocks")
     if len(MockV3Aggregator) <= 0:
-        mock_aggregator = MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": account}
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
         )
-    price_feed_address = MockV3Aggregator[-1].address
+    print("Mocks Deployed!")
